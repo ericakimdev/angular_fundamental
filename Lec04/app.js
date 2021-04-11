55002d960dd18e405f9f4c5c15538f66b3ff3d45
Erica <orange7961@gmail.com>
2021-04-11 00:31:07 -0400

    update lec04

---
+++ Lec04/app.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    var x = "hello"; //global scope
+
+    angular.module('myFirstApp', [])
+        .controller('MyFirstController', function () {
+
+        });
+
+})();
